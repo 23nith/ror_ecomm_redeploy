49fdccb0e1cef955e56d326aa848777e72ffe762
“23nith” <zenithmanuel@gmail.comgit config --global user.email ZENITHMANUEL@GMAIL.com>
2024-03-02 23:50:34 +0800

    Add Stripe webhook

--- config/routes.rb
+++ config/routes.rb
@@ -33,4 +33,5 @@ Rails.application.routes.draw do
   post "checkout" => "checkouts#create"
   get "success" => "checkouts#success"
   get "cancel" => "checkouts#cancel"
+  post "webhooks" => "webhooks#stripe"
 end

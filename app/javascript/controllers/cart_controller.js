76ffdd386e57b7dd79bd3413dd23de6ade266c09
“23nith” <zenithmanuel@gmail.comgit config --global user.email ZENITHMANUEL@GMAIL.com>
2024-03-02 19:10:53 +0800

    Create checkout logic

--- app/javascript/controllers/cart_controller.js
+++ app/javascript/controllers/cart_controller.js
@@ -42,5 +42,34 @@ export default class extends Controller {
     localStorage.setItem("cart", JSON.stringify(cart))
     window.location.reload()
   }
+
+  checkout(){
+    console.log("checkout")
+    const cart = JSON.parse(localStorage.getItem("cart"))
+    const payload = {
+      authenticity_token: "",
+      cart: cart
+    }
+
+    const csrfToken = document.querySelector("[name='csrf-token']").content
+
+    fetch("/checkout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": csrfToken
+      },
+      body: JSON.stringify(payload)
+    }).then(response => {
+      if(response.ok){
+        window.location.href = body.url
+      }else{
+        const errorEl = document.createElement("div")
+        errorEl.innerText = `There was an error processing your ordre. ${body.error}`
+        let errorContainer = document.getElementById("errorContainer")
+        errorContainer.appendChild(errorEl)
+      }
+    })
+  }
 }
 
